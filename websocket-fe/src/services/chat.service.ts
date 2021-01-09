6ba72fb45e5cb2dfa5e97a52d673de3546a3d209
Ardi Nasiri <ardashir.nasirabadi@gmail.com>
2021-01-09 16:29:10 +0100

    simple chat service

---
+++ websocket-fe/src/services/chat.service.ts
@@ -0,0 +1,48 @@
+import { SocketChat } from '../webSockets/socket.chat';
+import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
+
+type Data = {
+  sender: string;
+  room: string;
+  message: string;
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ChatService {
+  constructor(private socket: SocketChat) { }
+
+  public sendMessage(data: Data): void {
+    this.socket.emit('msgToServer', data);
+  }
+
+  public getMessages = () => {
+    return this.socket.fromEvent<Data>('msgToClient');
+  }
+
+  public joinRoom = (room: string): void => {
+    this.socket.emit('joinRoom', room);
+  }
+
+  public leaveRoom = (room: string): void => {
+    this.socket.emit('leaveRoom', room);
+  }
+
+  public joinedRoom = () => {
+    return this.socket.fromEvent('joinedRoom');
+  }
+
+  public leftRoom = () => {
+    return this.socket.fromEvent('leftRoom');
+  }
+
+  public onConnect = () => {
+    return this.socket.fromEvent('connect')
+      .pipe(
+        tap()
+      );
+  }
+}
